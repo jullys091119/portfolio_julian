5f1353c399f88fdfcf158b2c3fe81ad294199e5a
Julian <informacionlinux271087@gmail.com>
2024-02-05 18:09:27 -0700

    transicion de skills encima de branch mal

--- gsap.js
+++ gsap.js
@@ -10,11 +10,3 @@ export const showPublication = () => {
     gsap.fromTo(wrapperPublication, {opacity: 0}, {opacity: 1, duration: 1});
 }
 
-export const WelcomeText = (txt) => {
-    console.log(txt)
-    gsap.from(".description", {
-        duration: 5,
-        text: " ",
-        ease: "out",
-    });
-};
